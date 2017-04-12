451006c24def5f226a93891c92b8690079cd6925
periscuelo <periscuelo@gmail.com>
2017-04-12 17:12:42 -0300

    Adicionados exemplos utilizando Vue2

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # VueJs - Javascript - Quick Start
 
 Acho que ter apenas um arquivo utilizando VueJs 2.x com algumas linhas já fala por si só =)
+
+A pasta exemplos ECMA5 contem exemplos do VueJs sendo utilizado com js puro, sem transpilar.
+São 3 códigos diferentes e o ultimo deles usa Vuex para controlar dados entre os compontes.
+Está bem simples para quem deseja usar Vue sem webpack.
+
+O Vue2 não suporta URL Templates, ou seja, não possui um v-include para carregar templates.
+A razão é bem simples. Cada include seria equivalente a uma requisição ajax. Imagine dezenas
+de componentes em seu sistema cada um com um template via v-include. Agora faça isso vezes
+a quantidade de acessos ao sistema e você teria um problemão.
+
+Mas, pensando nisso, usei javascript com html para carregar um unico arquivo de templates
+x-template (do vue), sem ajax, de forma que todos os templates necessários possam ser carregados
+ao inicializar a aplicação.
+
+Caso não goste da solução, só lhe restará o webpack ou browserify. Neste caso usar o Single File
+Component do Vue será excelente. O Importante é saber que você tem opções e não é obrigado a nada
+com o Vue.
+
+Vale a pena experimentar o Vue2 =)
---
+++ exemplos ECMA5/vue_code3.js
@@ -0,0 +1,227 @@
+// tsc tsc
+var ready = function(fn) {
+  var interval = setInterval(function(){
+    if (document.readyState === 'complete') {
+      clearInterval(interval);
+      fn();
+    }
+  }, 1);
+};
+
+// Carrega o Template
+var loadTemplates = function(id) {
+  var selector = document.querySelector(id);
+  var scripts = selector.contentDocument.scripts;
+  var ct = scripts.length;
+  for (var i=0; i<ct; i++) {
+    document.body.appendChild(scripts[0]);
+  }
+  selector.parentNode.removeChild(selector);
+};
+
+// Adiciona o zero quando necessário
+var addZero = function(data) {
+  return (data < 10) ? '0' + data : data;
+}
+
+// Relógio
+var getAtime = function() {
+  var date = new Date();
+  var hour = addZero(date.getHours());
+  var minute = addZero(date.getMinutes());
+  var second = addZero(date.getSeconds());
+  var str_hour = hour + ':' + minute + ':' + second;
+  return str_hour;
+};
+
+Vue.component('relogio', {
+  template: '#tmpl_relogio',
+  data: function() {
+    return {
+      time: '00:00:00'
+    };
+  },
+  computed: {
+    aTime: function() {
+      var self = this;
+      setInterval(function() {
+        self.time = getAtime();
+      }, 1000);
+      return self.time;
+    }
+  }
+});
+// Fim Relógio
+
+//Vuex
+var store = new Vuex.Store({
+  state: {
+    values: []
+  },
+  mutations: {
+    mark: function(state, obj) {
+      var minute = addZero(obj.m);
+      var second = addZero(obj.s);
+      var milisecond = addZero(obj.ms);
+      state.values.push(minute + ':' + second + '.' + milisecond);
+      return state.values;
+    },
+    reset: function(state) {
+      state.values = [];
+      return state.values;
+    }
+  }
+});
+//Fim Vuex
+
+// Contador
+Vue.component('contador', {
+  template: '#tmpl_contador',
+  data: function() {
+    return {
+      tipo: 0,
+      mhour: 0,
+      mminute: 0,
+      msecond: 0,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      milisecond: 0,
+      started: false,
+      interval: '',
+      counter: '',
+      vcode: 3
+    };
+  },
+  computed: {
+    aCounter: function() {
+      var hour = addZero(this.hour);
+      var minute = addZero(this.minute);
+      var second = addZero(this.second);
+      var milisecond = addZero(this.milisecond);
+      switch (this.tipo) {
+        case '1':
+          this.counter = minute + ':' + second + '.<span class="h3">' + milisecond + '</span>';
+          break;
+        case '2':
+          this.counter = hour + ':' + minute + ':' + second;
+          break;
+        default:
+          this.counter = '';
+      }
+      return this.counter;
+    }
+  },
+  methods: {
+    count: function() {
+      var self = this;
+      self.started = true;
+
+      switch (self.tipo) {
+        case '1':
+          self.stopCount();
+          self.interval = setInterval(function() {
+            self.chronometer();
+          }, 10);
+          break;
+        case '2':
+          if (this.mhour > 0 || this.mminute > 0 || this.msecond > 0) {
+            self.stopCount();
+            self.interval = setInterval(function() {
+              self.temporizer();
+            }, 1000);
+          } else {
+            self.started = false;
+            alert('Informe quanto tempo deseja temporizar!');
+          }
+          break;
+      }
+
+      return self.counter;
+    },
+    stopCount: function() {
+      if (this.interval !== '') {
+        clearInterval(this.interval);
+        this.interval = '';
+      }
+    },
+    reset: function() {
+      this.stopCount();
+      this.mhour = 0;
+      this.mminute = 0;
+      this.msecond = 0;
+      this.hour = 0;
+      this.minute = 0;
+      this.second = 0;
+      this.milisecond = 0;
+      this.started = false;
+    },
+    mark: function() {
+      store.commit('mark', { m: this.minute, s: this.second, ms: this.milisecond });
+    },
+    chronometer: function() {
+      this.milisecond += 1;
+      if (this.milisecond === 100) {
+        this.milisecond = 0; 
+        this.second += 1;
+      }
+      if (this.second === 60) {
+        this.second = 0;
+        this.minute += 1;
+      }
+    },
+    temporizer: function() {
+      if (this.hour > 0 || this.minute > 0 || this.second > 0) {
+        if (this.second === 0 && this.minute === 0 && this.hour > 0) {
+          this.minute = 60;
+          this.hour -= 1;
+        }
+        if (this.second === 0 && this.minute > 0) {
+          this.second = 60;
+          this.minute -= 1;
+        }
+        this.second = (this.second === 0) ? 60 : this.second;
+        this.second -= 1;
+      }
+    }
+  },
+  watch: {
+    tipo: function() {
+      this.reset();
+    },
+    mhour: function(val) {
+      this.hour = parseInt(val, 10);
+    },
+    mminute: function(val) {
+      this.minute = parseInt(val, 10);
+    },
+    msecond: function(val) {
+      this.second = parseInt(val, 10);
+    }
+  }
+});
+// Fim Contador
+
+// Marcador
+Vue.component('marcador', {
+  template: '#tmpl_marcador',
+  computed: {
+    marcados: function() {
+      return store.state.values;
+    }
+  },
+  methods: {
+    clean: function() {
+      store.commit('reset');
+    }
+  }
+});
+// Fim Marcador
+
+// Instância Vue
+ready(function(){
+  loadTemplates('#templates');
+  new Vue({
+    el: '#app'
+  });
+});

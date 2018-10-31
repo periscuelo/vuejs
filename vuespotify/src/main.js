c8e98e0a4c8c693bc71411775e8fdf0b6d8389ad
periscuelo <periscuelo@gmail.com>
2018-10-31 12:52:46 -0300

    Atualizado eslint airbnb config

--- vuespotify/src/main.js
+++ vuespotify/src/main.js
@@ -4,9 +4,9 @@ import 'vuesax/dist/vuesax.css';
 import 'material-icons/iconfont/material-icons.css';
 
 import App from './App.vue';
-import router from './router';
 import mixins from './mixins';
-import store from './store/';
+import router from './router';
+import store from './store';
 
 Vue.use(Vuesax);
 Vue.mixin(mixins.utils);
